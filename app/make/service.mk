afed4a3f0f049fc18d42edfd6599b2c6683b9ff5
JuanP <84058843+JuanMontoyaP@users.noreply.github.com>
2026-02-04 15:33:31 -0500

    Improve docker-compose

--- app/make/service.mk
+++ app/make/service.mk
@@ -11,6 +11,8 @@ IMAGE_TAG=$(TAG_PREFIX)-$(DATE_TAG)-$(GIT_SHA)
 AWS_REGION?=us-east-1
 AWS_ACCOUNT:=$(shell aws sts get-caller-identity --query Account --output text)
 
+.PHONY: sync lock dev build migrations run logs shell db clean
+
 sync: ## Sync Dependencies on the environment
 	uv sync
 
@@ -20,7 +22,7 @@ lock: ## Lock dependencies
 dev: sync lock db ## Run the service in local
 	uv run uvicorn src.main:app --reload --host 0.0.0.0 --port $(PORT)
 
-build: ## Build Docker image
+build: sync lock ## Build Docker image
 	docker build \
 	--platform linux/amd64 \
 	--provenance=false \
@@ -28,11 +30,21 @@ build: ## Build Docker image
 	-t $(IMAGE_NAME):$(IMAGE_TAG) \
 	.
 
-run: build ## Run Docker container in local
-	docker run --env-file .env --rm -p $(PORT):80 $(IMAGE_NAME):$(IMAGE_TAG)
+migrations:
+	uv run alembic upgrade head
+
+run: sync lock ## Run development containers
+	docker-compose up -d && uv run alembic upgrade head
+
+logs: run ## See app logs
+	docker-compose logs -f $(SERVICE)
+
+shell: ## Run shell on container development
+	docker-compose exec $(SERVICE) /bin/bash
 
 db: ## Run local DB for development
 	docker-compose -f ../docker-compose.db.yml up -d
 
 clean: ## Clean all containers
+	docker-compose down -v
 	docker-compose -f ../docker-compose.db.yml down -v
